6230db9197b4c32391883d2e483797cca359e4aa
Taras Pyvovarov <taras.pyvovarov@gmail.com>
2016-01-10 17:31:08 +0200

    Inited wpf app basics

---
+++ Module.People/Module.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Practices.Unity;
+using Module.People.Views;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Module.People
+{
+    public class Module : IModule
+    {
+        private readonly IUnityContainer container;
+        private readonly IRegionManager regionManager;
+
+        public Module(IUnityContainer container, IRegionManager regionManager)
+        {
+            this.container = container;
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            // Register the EmployeeDataService concrete type with the container.
+            // Change this to swap in another data service implementation.
+            //this.container.RegisterType<IEmployeeDataService, EmployeeDataService>();
+
+            // View will be automatically injected to the region when the region is first displayed.
+            this.regionManager.RegisterViewWithRegion("LogoRegion", () => this.container.Resolve<LogoView>());
+            //this.regionManager.RegisterViewWithRegion("LeftRegion", () => this.container.Resolve<EmployeeDetailsView>());
+            //this.regionManager.RegisterViewWithRegion("MainRegion", () => this.container.Resolve<EmployeeDetailsView>());
+            //this.regionManager.RegisterViewWithRegion("StatusRegion", () => this.container.Resolve<EmployeeProjectsView>());
+        }
+    }
+}
---
+++ Ref.UI/Infrastructure/UICompositionBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using AppUI.Views;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace AppUI
+{
+    internal class UICompositionBootstrapper : UnityBootstrapper
+    {
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(Module.People.Module));
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            // Use the container to create an instance of the shell.
+            ShellView view = this.Container.TryResolve<ShellView>();
+            return view;
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
