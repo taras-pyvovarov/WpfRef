07f5a6f007332cde012ad0f441eb392e23c9e466
Taras <taras.pyvovarov@globallogic.com>
2016-01-14 08:08:56 +0200

    Added textbox validation

--- Module.People/ViewModels/RegionViewModels/MainPanelViewModel.cs
+++ Module.People/ViewModels/RegionViewModels/MainPanelViewModel.cs
@@ -14,10 +14,11 @@ using Module.People.ViewModels;
 using Module.People.Views;
 using Prism.Modularity;
 using Prism.Regions;
+using System.ComponentModel;
 
 namespace Module.People.ViewModels
 {
-    public class MainPanelViewModel : BindableBase
+    public class MainPanelViewModel : BindableBase, IDataErrorInfo
     {
         private IUnityContainer _container;
 
@@ -44,7 +45,21 @@ namespace Module.People.ViewModels
             set { SetProperty(ref _dialogCommand, value); }
         }
 
+        public ICommand _passEventParamsCommand;
+        public ICommand PassEventParamsCommand
+        {
+            get { return _passEventParamsCommand; }
+            set { SetProperty(ref _passEventParamsCommand, value); }
+        }
+
         #endregion Commands
+
+        public string _validatedText;
+        public string ValidatedText
+        {
+            get { return _validatedText; }
+            set { SetProperty(ref _validatedText, value); }
+        }
 
         public MainPanelViewModel(IUnityContainer container)
         {
@@ -53,6 +68,7 @@ namespace Module.People.ViewModels
             BlankLongCommand = new DelegateCommand(BlankLongExecute);
             BlankLongAsyncCommand = DelegateCommand.FromAsyncHandler(BlankLongAsyncExecute);
             DialogCommand = new DelegateCommand(DialogExecute);
+            PassEventParamsCommand = new DelegateCommand(PassEventParamsExecute);
         }
 
         #region Command executes
@@ -73,11 +89,61 @@ namespace Module.People.ViewModels
             windowService.ShowDialog(new ActionPanelViewModel(), _container.Resolve<Dictionary<Type, Type>>());
         }
 
+        private void PassEventParamsExecute()
+        {
+            
+        }
+
         #endregion Command executes
 
         private void HeavyOperation()
         {
             Thread.Sleep(5000);
         }
+
+        private string Validate(string propertyName)
+        {
+            // Return error message if there is error on else return empty or null string
+
+            string validationMessage = null;
+            switch (propertyName)
+            {
+                case nameof(ValidatedText):
+                    if (ValidateValidatedText())
+                        return null;
+                    validationMessage = "Error";
+                    break;
+            }
+
+            return validationMessage;
+        }
+
+        private bool ValidateValidatedText()
+        {
+            if (string.IsNullOrEmpty(ValidatedText))
+                return true;
+
+            foreach (var singleChar in ValidatedText)
+            {
+                if (singleChar < '1' || singleChar > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        #region IDataErrorInfo implementation
+
+        public string Error
+        {
+            get { return "Error"; }
+        }
+
+        public string this[string columnName]
+        {
+            get { return Validate(columnName); }
+        }
+
+        #endregion IDataErrorInfo implementation
     }
 }
